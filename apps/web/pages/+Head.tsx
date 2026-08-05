4c63fb59571abebcdff2a8c56ac9df028daed8b0
iplanwebsites <787729+iplanwebsites@users.noreply.github.com>
2026-08-05 11:19:16 -0400

    add Rybbit analytics

--- apps/web/pages/+Head.tsx
+++ apps/web/pages/+Head.tsx
@@ -7,6 +7,7 @@ export default function Head() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+      <script src="https://ry.secretdef.com/script.js" data-site-id="9317848ba9c2" defer />
     </>
   );
 }
